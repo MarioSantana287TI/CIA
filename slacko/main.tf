f192864510463cf842540b7becb54bff0f4f2d36
MarioSantana287TI <mario@doisoitosete.com>
2021-09-27 20:24:58 -0300

    Atividade slacko - MBA CIA

--- slacko/main.tf
+++ slacko/main.tf
@@ -13,7 +13,6 @@ data "aws_ami" "slacko-app" {
     }
 }
 
-# data para capturar id da vpc automaticamente baseado no nome my-vpc
 data "aws_vpc" "my-vpc" {
   filter {
     name = "tag:Name"
@@ -27,8 +26,7 @@ data "aws_subnet" "subnet_public"{
 
 resource "aws_key_pair" "slacko-sshkey" {
     key_name = "slacko-app-key"
-    #Informe a sua chave SSH aqui!!!
-    public_key = "****"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFyEp/yNg7euy5jq5dMLGO0DtogLDA0VbP9DDJnmHgNd2d6VCbzB/f4J++BvZbFv9qd6LZbMo2KibqH4GnisNW3VfyuNcsW4s9Izx/kfyxqze35aIjYCLoM3QgfiChTfJK7mwOl+6Ce1/+Kql87zfthik9MVVG8H/QNme8/JGgaJBVFCp5lwUXgg5awAmRFKO7tdJRGQ/rQaIFD3MqQjdXO5vPgx/oPHshD9lsER6mR+4P6rNIqdIBInVwPk7Mkk+3OcrOBwq3Um9g0ZfUSYI0f6jErP2v1PHPVG/V4UAYEDuQzPwgdaU4Cdjt7iLnYD4ln5eziuaZPtWVi/i6fRokoWEOSLpxDluuIGL9Cn60CvqxZoN5fJ8cU+MMXkbCjVnwgMaeDdWAhK01WHXrk0C4ODFzsksC8hoCU0NhyIzyIE/5vw0KmKVHZ1jHqSrSLkOL8i4L7xQ12OYOZz9g1mr6t4NT7IYYQ8VJifCSXXx/e8hQZPP96q1en2q5PvOg5M8= mario@ubuntu"
 }
 
 resource "aws_instance" "slacko-app" {
